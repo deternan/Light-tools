84caee2582c8af5b100b6d26c11702f5db570449
deternan <chke@iir.csie.ncku.edu.tw>
2017-01-03 00:50:34 +0800

    Minor revised (download function)

--- src/Auto_download.java
+++ src/Auto_download.java
@@ -16,11 +16,18 @@
  * GNU General Public License for more details.
  * 
  * 
- * Last revision: December 31, 2016	12:30 PM
+ * Last revision: January 02, 2017	11:56 PM
  */
 
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -29,28 +36,60 @@ import org.jsoup.nodes.Element;
 public class Auto_download 
 {
 	private String Source_ = "http://www.ored.com.tw/AUTHAdmin_SJW/file_upload/";
-	private String Target_folder = "C:\\Users\\user\\Desktop\\Constract\\";
+	private String Target_folder = "D:\\Constract\\";
 	    
 	// 
 	private OutputStream outStream_file;
-    
+    // WWW
+	private int port = 80;
+	
 	public Auto_download() throws Exception
 	{
+		String enc = System.getProperty("file.encoding");
+		System.out.println(enc);
 		
+
+		String filename;
 		Document doc = Jsoup.connect(Source_).get();
-        for (Element file : doc.select("a"))
+        String UTF_8_str;
+		for (Element file : doc.select("a"))
         {
-            System.out.println(file.attr("href"));
+            //System.out.println(count+++"	"+file.attr("href"));
+			filename = file.attr("href").toString().substring(file.attr("href").toString().lastIndexOf("/")+1, file.attr("href").toString().length());
+			UTF_8_str = new String(filename.getBytes("UTF-8"), "UTF-8"); 
+            
+            System.out.println(UTF_8_str);
+            if((UTF_8_str!=null)&&(UTF_8_str.length() >0)){
+            	Download(UTF_8_str);
+            }            
         }
 		
 	}
 	
-	private void Download_file(String remoteFile) 
-	{
-		String temp;		
-		String nxml_tar[] = remoteFile.split("/");
+	private void Download(String filename) throws Exception
+	{		
+		System.out.println(Target_folder + filename);
+		URL url = new URL(Source_ + filename);
+		InputStream in = url.openStream();
+		FileOutputStream fos = new FileOutputStream(new File(Target_folder + filename));				
 		
-			
+		int length = -1;
+		byte[] buffer = new byte[1024];// buffer for portion of data from connection
+		while ((length = in.read(buffer)) > -1) 
+		{
+		    fos.write(buffer, 0, length);
+		}
+		fos.close();
+		in.close();
+		System.out.println(filename+" File downloaded");
+		
+		
+		/*
+		URL url = new URL(Source_ + filename);		
+		InputStream in = url.openStream();
+		Files.copy(in, Paths.get(Target_folder + filename), StandardCopyOption.REPLACE_EXISTING);
+		in.close();
+		*/
 	}
 	
 	public static void main(String args[])
@@ -63,3 +102,4 @@ public class Auto_download
 		}
 	}
 }
+
