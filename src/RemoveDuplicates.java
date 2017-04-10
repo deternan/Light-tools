7d3a9c754b21558c70a788b043d6b811d07e7c80
deternan <chke@iir.csie.ncku.edu.tw>
2017-04-10 15:44:47 +0800

    Remove Duplicates

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,51 @@
+
+/*
+ *  RemoveDuplicates (Array)
+ * 
+ * Version: April 10, 2017 03:32 PM
+ * Last revision: April 10, 2017 03:32 PM 
+ * 
+ * Author : Chao-Hsuan Ke (Phelps Ke)
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc., Taiwan
+ *  
+ */
+
+public class RemoveDuplicates 
+{
+	private int[] all = {1, 2, 2, 3, 9, 6, 8, 9, 12, 7, 15};
+	
+	public RemoveDuplicates()
+	{
+		int all_length = all.length;
+
+	    for (int i = 0; i < all_length; i++) 
+	    {
+	        for (int j = i + 1; j < all_length; j++) 
+	        {
+	            if (all[i] == all[j]) {                  
+	                int shiftLeft = j;
+	                for (int k = j+1; k < all_length; k++, shiftLeft++) 
+	                {
+	                	all[shiftLeft] = all[k];
+	                }
+	                all_length--;
+	                j--;
+	            }
+	        }
+	    }
+
+	    int[] remove_list = new int[all_length];
+	    for(int i = 0; i < all_length; i++)
+	    {
+	    	remove_list[i] = all[i];
+	    }
+	   
+	}
+	
+	public static void main(String args[])
+	{
+		RemoveDuplicates RD = new RemoveDuplicates();
+	}
+	
+}
