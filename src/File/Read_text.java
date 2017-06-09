401196bc9f3ae0f2c50d7bd415de88a1aa12aea3
deternan <chke@iir.csie.ncku.edu.tw>
2017-06-09 10:29:35 +0800

    read file

---
+++ src/File/Read_text.java
@@ -0,0 +1,38 @@
+package File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Read_text 
+{
+	private String folder = "";
+	private String file = "";
+	
+	private BufferedReader bfr;
+	private StringBuffer sb = new StringBuffer();
+	
+	public Read_text() throws Exception
+	{
+		String Line = "";
+		FileReader fr = new FileReader(folder + file);
+		bfr = new BufferedReader(fr);
+		
+		while((Line = bfr.readLine())!=null)
+		{
+			sb.append(Line+"\n");
+		}
+		fr.close();
+		bfr.close();
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+			Read_text rt = new Read_text();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
