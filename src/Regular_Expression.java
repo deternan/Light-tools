2b53e73cbe657cfe59c0e0e15ca174a5632e606b
deternan <chke@iir.csie.ncku.edu.tw>
2017-07-09 19:28:13 +0800

    added space check

--- src/Regular_Expression.java
+++ src/Regular_Expression.java
@@ -4,23 +4,36 @@ import java.util.regex.Pattern;
 /*
  * http://www.geeksforgeeks.org/regular-expressions-in-java/
  * 
- * 
- * Last revision: July 09, 2017 04:40 PM
+ * Last revision: July 09, 2017 07:25 PM
  * 
  */
 
 public class Regular_Expression 
 {
-	String pattern = "[0-9]+";	
-	String temp_str = "056894";
+	Pattern p;
+	Matcher m;
+	
+	String num_pattern = "[0-9]{4}";
+	String space_pattern = "^[0-9]";
+			
+	String temp_str = "1102　";
 
 	public Regular_Expression()
 	{
-		Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(temp_str);
+		// Number check
+		p = Pattern.compile(num_pattern);
+        m = p.matcher(temp_str);
         
         while(m.find()){
         	System.out.println(m.group());
+        }
+        
+        // Space check
+        String temp = temp_str.substring(4, 5);
+        p = Pattern.compile(space_pattern);
+        m = p.matcher(temp_str);
+        while(m.find()){
+        	System.out.println("Not Digital");
         }
 	}
 	
