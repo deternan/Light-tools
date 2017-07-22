a98da9a59fbca62bd4cfc797bcf1d198f7f1bb0b
deternan <chke@iir.csie.ncku.edu.tw>
2017-07-22 16:09:48 +0800

    Get list of files/directories of an directory url

---
+++ src/html_download/Web_folder.java
@@ -0,0 +1,40 @@
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/*
+ * 
+ * Get web folder list
+ * Copyright (C) 2017 Phelps Ke, phelpske.dev at gmail dot com
+ * 
+ * Last revision: July 22, 2017 03:59 PM
+ * 
+ * JAR
+ * Jsoup-1.10.2.jar
+ */
+
+public class Web_folder 
+{
+	String url = "http://fund.bot.com.tw/z/ze/zeb/";
+	
+	public Web_folder() throws Exception
+	{
+		Document doc = Jsoup.connect(url).get();
+        for (Element file : ((Element) doc).select("td.right td a")) {
+            System.out.println(file.attr("href"));
+        }
+	}
+	
+	public static void main(String args[])
+	{
+		try {
+			Web_folder wf = new Web_folder();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
