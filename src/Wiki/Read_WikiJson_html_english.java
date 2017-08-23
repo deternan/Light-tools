93aecb82af82307d34fd401b1e12bb43f2913a45
deternan <chke@iir.csie.ncku.edu.tw>
2017-08-23 14:49:07 +0800

    Get wikitext from html (English words)

--- src/Wiki/Read_WikiJson_html_english.java
+++ src/Wiki/Read_WikiJson_html_english.java
@@ -14,7 +14,7 @@ import org.json.JSONObject;
 /*
  * 
  * version: August 22, 2017 02:24 PM
- * Last revision: August 23, 2017 01:59 PM
+ * Last revision: August 22, 2017 05:26 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -29,6 +29,8 @@ public class Read_WikiJson_html_english
 
 	JSONObject get_json = null;
 	private String wikitext_all;
+	private Vector wikitext_vec = new Vector();
+	private String wikitext_english;
 	
 	public Read_WikiJson_html_english()
 	{
@@ -36,8 +38,12 @@ public class Read_WikiJson_html_english
 		readJsonFromUrl("https://en.wikipedia.org/w/api.php?action=query&redirects=&converttitles=&prop=revisions&rvprop=content&format=json&titles=" + input_title);
 		// Parser Json
 		Parser(get_json);
-				
-		System.out.println(wikitext_all);
+		//System.out.println(get_json);
+		// Filter
+		Separation();
+		// Get English words
+		wikitext_english = Text_filter();
+		System.out.println(wikitext_english);
 	}
 	
 	private void readJsonFromUrl(String url)  
@@ -94,7 +100,75 @@ public class Read_WikiJson_html_english
 			e.printStackTrace();
 		}		
 	}
+	
+	private void Separation()
+	{
+		String[] temp;
+		temp = wikitext_all.split("\\n");
+		//System.out.println(wikitext_all);		
+		//System.out.println(temp.length);
 		
+		String front_term;
+		String temp_;
+		for(int i=0; i<temp.length; i++)
+		{
+			if(temp[i].contains(".")){
+				front_term = temp[i].substring(0, 1);				
+				if(English_filter(front_term)){
+					temp_ = Remove_tag(temp[i]);
+					//System.out.println(English_filter(front_term)+"	"+temp_);
+					
+					wikitext_vec.add(temp_);
+				}				
+			}			
+		}
+	}
+	
+	private boolean English_filter(String input_str)
+	{
+		boolean check;		
+		check = input_str.matches("[a-zA-Z]+");
+		
+		return check;
+	}
+	
+	private String Remove_tag(String input_str)
+	{
+		input_str = input_str.replace("[[", "");
+		input_str = input_str.replace("]]", "");
+		input_str = input_str.replace("''", "");
+		input_str = input_str.replace("\\\"", "");
+		int ref_index = input_str.indexOf("<ref");
+		int ref_end = input_str.indexOf("</ref>");
+		if((ref_index>0) && (ref_end>0)){
+			String ref_str = input_str.substring(ref_index, ref_end+"</ref>".length());
+			//System.out.println(ref_str);
+			input_str = input_str.replace(ref_str, "");			
+		}
+		
+		return input_str;
+	}
+	
+	private String Text_filter()
+	{
+		String sentence = "";
+		
+		for(int i=0; i<wikitext_vec.size(); i++)
+		{
+			sentence += wikitext_vec.get(i).toString()+"\\s";
+			//for(int j=0; j<wikitext_vec.get(i).toString().length(); j++)
+			{  
+				
+//			    String test = wikitext_vec.get(i).toString().substring(j, j+1);		    
+//			    if(test.matches("[{0-9}{a-z}{A-Z}.]+")){			        
+//			    	
+//			    }  		      
+			}	
+		}
+		
+		return sentence;
+	}
+	
 	public static void main(String args[]) 
 	{
 		Read_WikiJson_html_english read = new Read_WikiJson_html_english();
