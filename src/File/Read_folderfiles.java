4d59f677095442ef169dce12bd6ffac941478a69
deternan <chke@iir.csie.ncku.edu.tw>
2017-08-23 18:11:42 +0800

    Read files in folder

---
+++ src/File/Read_folderfiles.java
@@ -0,0 +1,37 @@
+package File;
+
+/*
+ * 
+ * version: August 23, 2017 06:02 PM
+ * Last revision: August 23, 2017 06:02 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+import java.io.File;
+
+public class Read_folderfiles 
+{
+	private String folder_source = "C:\\Users\\Barry.Ke\\Desktop\\wikitext\\chinese\\";
+	
+	public Read_folderfiles()
+	{
+		File folder = new File(folder_source);
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		        System.out.println(file.getName());
+		    }
+		}
+	}
+	
+	public static void main(String args[]) 
+	{
+		Read_folderfiles folderfiles = new Read_folderfiles();
+	}
+	
+}
