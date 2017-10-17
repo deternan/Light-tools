44b6b33257b13f34595529d4a1c43ede731e71f9
deternan <chke@iir.csie.ncku.edu.tw>
2017-10-17 13:26:06 +0800

    API (GET)

--- src/API/Get.java
+++ src/API/Get.java
@@ -4,9 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,12 +22,20 @@ import org.json.JSONObject;
  * Get API
  * 
  * version: October 17, 2017 11:13 AM
- * Last revision: October 17, 2017 11:13 AM
+ * Last revision: October 17, 2017 01:18 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
  * Company : Delta Electronics Inc. (Taiwan)
  * 
+ */
+
+/*
+ * JAR
+ * httpclient-4.5.1.jar
+ * httpcore-4.4.6.jar
+ * apache-commons.jar
+ * slf4j-api-1.7.12.jar
  */
 
 public class Get 
@@ -37,22 +52,51 @@ public class Get
 	
 	public Get() throws Exception
 	{
+		//version_1();
+		version_2();        
+	}
+	
+	private void version_1() throws Exception
+	{
+		// version 1
 		URL obj = new URL(get_url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();		
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		// optional default is GET
 		con.setRequestMethod("GET");
-		//add request header
+		// add request header
 		con.setDoOutput(true);
-        con.setRequestProperty  ("Authorization", "Bearer " + token);
-        InputStream content = (InputStream)con.getInputStream();
-        BufferedReader in = new BufferedReader (new InputStreamReader (content));
-        String line;
-        while ((line = in.readLine()) != null) 
-        {
-            System.out.println(line);
-            //Parser(line);
-        }  
-		
+		con.setRequestProperty("Authorization", "Bearer " + token);
+		InputStream content = (InputStream) con.getInputStream();
+		BufferedReader in = new BufferedReader(new InputStreamReader(content));
+		String line;
+		while ((line = in.readLine()) != null) {
+			System.out.println(line);
+			// Parser(line);
+		}
+	}
+	
+	private void version_2() throws Exception
+	{
+		// version 2        		
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(get_url);
+
+		// add request header
+		request.addHeader("User-Agent", USER_AGENT);
+
+		HttpResponse response = client.execute(request);
+
+		System.out.println("\nSending 'GET' request to URL : " + get_url);
+		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		System.out.println(result.toString());        
 	}
 	
 	private void Parser(String input) throws JSONException
