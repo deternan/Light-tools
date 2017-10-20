6eed409ec5a3ef7c53e600d235bca7d3dfd06951
deternan <chke@iir.csie.ncku.edu.tw>
2017-10-20 13:55:56 +0800

    Read folder

---
+++ src/File/Readfoler.java
@@ -0,0 +1,40 @@
+package File;
+
+import java.io.File;
+
+/*
+ * Read folder list
+ * 
+ * version: October 20, 2017 01:41 PM
+ * Last revision: October 20, 2017 01:41 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+public class Readfoler 
+{
+	private String dirPath = "";
+	
+	public Readfoler()
+	{
+		
+		File dir = new File(dirPath);
+		String[] files = dir.list();
+		if (files.length == 0) {
+		    System.out.println("The directory is empty");
+		} else {
+		    for (String folername : files) {
+		        System.out.println(folername);
+		    }
+		}
+	}
+	
+	public static void main(String args[]) 
+	{
+		Readfoler r_folder = new Readfoler();
+	}
+	
+}
