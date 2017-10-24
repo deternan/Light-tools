ac742e693598c67e48dcbf95038bfb38eafeec2e
deternan <chke@iir.csie.ncku.edu.tw>
2017-10-24 11:36:36 +0800

    based on Hash

--- src/RemoveDuplicates_vector.java
+++ src/RemoveDuplicates_vector.java
@@ -1,5 +1,17 @@
-import java.util.LinkedHashSet;
 import java.util.Vector;
+
+
+/*
+ * Reonve duplication (vector) 
+ * 
+ * version: October 24, 2017 11:33 AM
+ * Last revision: October 24, 2017 11:33 AM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
 
 public class RemoveDuplicates_vector 
 {
@@ -11,16 +23,19 @@ public class RemoveDuplicates_vector
 		test.add("Pineapple");
 		test.add("apple");
 		test.add("pen");	
-		
-		Vector newVect = new Vector(new LinkedHashSet(test));
-		
-		
-		for(int i=0; i<newVect.size(); i++)
-		{
-			System.out.println(newVect.get(i));
+				
+		for (int i = 0; i < test.size(); i++) {
+			for (int j = 0; j < test.size(); j++) {
+				if (i != j) {
+					if (test.elementAt(i).equals(test.elementAt(j))) {
+						test.removeElementAt(j);
+					}
+				}
+			}
 		}
+		
 	}
-	
+
 	public static void main(String args[])
 	{
 		RemoveDuplicates_vector rv = new RemoveDuplicates_vector();
