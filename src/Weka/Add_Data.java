9307805d9192abba7bdf9fed8ea02cfd893a7f3b
deternan <chke@iir.csie.ncku.edu.tw>
2017-12-12 18:43:44 +0800

    no message

--- src/Weka/Add_Data.java
+++ src/Weka/Add_Data.java
@@ -1,5 +1,13 @@
 package Weka;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
@@ -25,6 +33,15 @@ import weka.filters.unsupervised.attribute.Add;
 
 public class Add_Data 
 {
+	// Parameters
+	private final int dim = 10;
+	
+	double[] instnace_num = new double[dim];
+	
+	// output
+	private String output_folder = "C:\\Users\\Barry.Ke\\Desktop\\";
+	private String output_file = "AAA.txt";		
+	String instnace_str = "";
 	
 	public Add_Data()
 	{
@@ -32,22 +49,21 @@ public class Add_Data
 //		Attribute length = new Attribute("length"); 
 //		Attribute weight = new Attribute("weight"); 
 
-		//Attributes();		
-		//Class();
+		Attributes();		
+		Class();
 		Instances();
 	}
 	
 	private void Attributes()
 	{
-		Attribute a1 = new Attribute("sepallength", 0);
-        Attribute a2 = new Attribute("REAL", 1);       
+		
+        Attribute a1 = new Attribute("REAL", 1);       
         
         FastVector attrs = new FastVector();
-		attrs.addElement(a1);
-		attrs.addElement(a2);
+		attrs.addElement(a1);		
 		
 		System.out.println(attrs.get(0));
-		System.out.println(attrs.get(1));
+		
 	}
 	
 	private void Class()
@@ -61,12 +77,45 @@ public class Add_Data
 		// Create nominal attribute "position"
 		Attribute position = new Attribute("class", my_nominal_values);
 
-		System.out.println(position);
+		System.out.println(position.getRevision());
 	}
 	
 	private void Instances()
 	{
+//		Instance newInstance = null;
+//		for(int i = 0 ; i<10 ; i++)
+//		{
+//		    newInstance.setValue(i , 0.5);
+//		    //i is the index of attribute
+//		    //value is the value that you want to set
+//		 }
+//		 //add the new instance to the main dataset at the last position
+//		System.out.println(newInstance);
 		
+		Random rand = new Random();
+		
+		for(int i=0; i<instnace_num.length; i++)
+		{
+			instnace_num[i] = rand.nextInt();
+			instnace_str += String.valueOf(instnace_num[i]+",");
+		}
+		
+	}
+	
+	private void output()
+	{
+		BufferedWriter writer;
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output_folder + output_file), "utf-8"));
+			writer.write("QQQ");
+			writer.close();
+		} catch (UnsupportedEncodingException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args)
