1f50829122485a9b273a5d4ab113be2bb25554d7
deternan <chke@iir.csie.ncku.edu.tw>
2017-12-13 19:01:21 +0800

    html parser

---
+++ src/html/Html_Parser.java
@@ -0,0 +1,50 @@
+package html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/*
+ * html parser 
+ * 
+ * version: December 13, 2017 05:25 PM
+ * Last revision: December 13, 2017 07:00 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ */
+
+/*
+ * JAR
+ * jsoup-1.10.2.jar
+ */
+
+public class Html_Parser 
+{
+	String url = "https://technews.tw/2017/12/12/apple-app-pre-order/";
+	
+	public Html_Parser() throws Exception
+	{		
+		Document doc = Jsoup.connect(url).get();
+        
+		// title
+		String title = doc.title();
+        System.out.println(title);
+		
+        // Text
+        Elements entrycontent = doc.getElementsByClass("entry-content");
+        System.out.println(entrycontent.text());
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+			Html_Parser htmlparser = new Html_Parser();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
