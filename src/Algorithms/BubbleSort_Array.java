e366fe0a9430d44569a65989cf3569da277a4b1d
deternan <chke@iir.csie.ncku.edu.tw>
2018-01-25 18:53:28 +0800

    Bobble Sort (Array)

---
+++ src/Algorithms/BubbleSort_Array.java
@@ -0,0 +1,54 @@
+package Algorithms;
+
+/*
+ * Bobble Sort (Array)
+ * 
+ * version: January 25, 2018 06:39 PM
+ * Last revision: January 25, 2018 06:39 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+public class BubbleSort_Array 
+{
+
+	public BubbleSort_Array(double[] input)
+	{
+		int lenD = input.length;
+		int j = 0;
+		double tmp = 0;
+		String videoid_temp = "";
+		String videourl_temp = "";
+		
+		for(int i=0; i<lenD; i++)
+		{
+		    j = i;
+		    for(int k=i; k<lenD; k++)
+		    {
+		      if(input[j] < input[k]){
+		        j = k;		        
+		      }
+		    }
+		    // Numerical
+		    tmp = input[i];		   
+		    input[i] = input[j];
+		    input[j] = tmp;		   
+		}
+		
+		for(int i=0; i<input.length; i++)
+		{
+			System.out.println(input[i]);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		double[] input = {88, 81, 74, 68, 78, 76, 77, 85, 95, 93};
+		
+		BubbleSort_Array BS_array = new BubbleSort_Array(input);
+	}
+	
+}
