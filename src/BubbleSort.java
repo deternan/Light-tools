2c7e7b6adaf9b4fe0b0d92bf59fc6d1f3776a937
deternan <chke@iir.csie.ncku.edu.tw>
2018-02-12 22:49:55 +0800

    BubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort 
+{
+
+	public BubbleSort(double[] input)
+	{
+		int lenD = input.length;
+		int j = 0;
+		double tmp = 0;
+		
+		for(int i=0; i<lenD; i++)
+		{
+		    j = i;
+		    for(int k=i; k<lenD; k++)
+		    {
+		      if(input[j] < input[k]){
+		        j = k;		        
+		      }
+		    }
+		    // Numerical
+		    tmp = input[i];		   
+		    input[i] = input[j];
+		    input[j] = tmp;		   
+		}
+	}
+	
+}
