f2a661703e8a3fe2771a5429560020327ce4cb67
deternan <chke@iir.csie.ncku.edu.tw>
2018-06-02 13:37:46 +0800

    regular expression (numerical)

--- src/RegularExpression/RegularExpression_Number.java
+++ src/RegularExpression/RegularExpression_Number.java
@@ -1,9 +1,12 @@
 package RegularExpression;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
  * 
  * version: June 01, 2018 05:54 PM
- * Last revision: June 01, 2018 05:54 PM
+ * Last revision: June 02, 2018 01:35 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -13,14 +16,21 @@ package RegularExpression;
 
 public class RegularExpression_Number 
 {
-
+	String regex = "[0-9]+";
+	Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+	
 	public RegularExpression_Number()
-	{
-		String regex = "[0-9]+";
-		String data = "1234567";
+	{		
+		String data = "1234aa567";
 		
+		// True/false
 		System.out.println(data.matches(regex));
-		
+		// Parsing
+		Matcher matcher = pattern.matcher(data);        
+        while(matcher.find())
+        {
+        	System.out.println(matcher.group());
+        }
 	}
 	
 	public static void main (String args[])
