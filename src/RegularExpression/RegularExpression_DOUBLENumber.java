58d6ffc7a36f0fffc4d4107d3bfb694a3231d9e3
deternan <chke@iir.csie.ncku.edu.tw>
2018-06-02 13:42:15 +0800

    regular expression (double)

---
+++ src/RegularExpression/RegularExpression_DOUBLENumber.java
@@ -0,0 +1,37 @@
+package RegularExpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * 
+ * version: June 01, 2018 05:54 PM
+ * Last revision: June 02, 2018 01:39 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * 
+ */
+
+public class RegularExpression_DOUBLENumber 
+{
+	String regex = "[0-9]+\\.?[0-9]+";
+	Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+	
+	public RegularExpression_DOUBLENumber()
+	{		
+		String data = "12.45aa69.7";
+		
+		// Parsing
+		Matcher matcher = pattern.matcher(data);        
+        while(matcher.find())
+        {
+        	System.out.println(matcher.group());
+        }
+	}
+	
+	public static void main (String args[])
+	{
+		RegularExpression_DOUBLENumber RE_num = new RegularExpression_DOUBLENumber();
+	}
+	
+}
