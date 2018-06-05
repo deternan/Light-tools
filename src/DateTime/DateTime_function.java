2f8f0aa2a2696416b2af18e0db7057f3fd6c1f6f
deternan <chke@iir.csie.ncku.edu.tw>
2018-06-05 09:18:52 +0800

    new folder

---
+++ src/DateTime/DateTime_function.java
@@ -0,0 +1,123 @@
+package DateTime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.lucene.document.DateTools;
+import org.apache.lucene.document.DateTools.Resolution;
+
+/*
+ * version: February 22, 2018 06:23 PM
+ * Last revision: February 26, 2018 05:19 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+public class DateTime_function 
+{
+	DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+	
+	Pattern p;
+	Matcher m;
+	
+	// Week days
+	String week_start;
+	
+	public DateTime_function()
+	{
+		// time to string 
+			// example :  2018-02		
+		String date_str = Time_to_String("2018-02");
+		System.out.println(date_str);
+						
+		//Week_Days(2018,02,21);
+		ArrayList<String> day_array;
+		day_array = Date_separation("2018-02-18");
+		Week_Days(Integer.parseInt(day_array.get(0)), Integer.parseInt(day_array.get(1)), Integer.parseInt(day_array.get(2)));
+		
+		// Today
+		Today();
+	}
+	
+	private String Time_to_String(String input)
+	{
+		String year_str = input.substring(0, input.indexOf("-"));
+		String month_str = input.substring(input.indexOf("-")+1, input.length());
+		int year = Integer.parseInt(year_str);
+		int month = Integer.parseInt(month_str);
+		
+		Calendar myCal = Calendar.getInstance();
+		myCal.set(Calendar.YEAR, year);
+		myCal.set(Calendar.MONTH, month-1);
+		myCal.set(Calendar.DAY_OF_MONTH, 01+1);
+		myCal.set(Calendar.HOUR_OF_DAY, 0);
+		myCal.set(Calendar.MINUTE, 0);
+		myCal.set(Calendar.SECOND, 0);
+		Date theDate = myCal.getTime();		
+		DateTools.dateToString(theDate, Resolution.MONTH);		
+		//System.out.println(DateTools.dateToString(theDate, Resolution.DAY));				
+		
+		return DateTools.dateToString(theDate, Resolution.DAY);
+	}
+	
+	private ArrayList Date_separation(String input)
+	{		
+		Date return_date = null;
+		ArrayList<String> obj = new ArrayList<String>();
+		
+		String pattern = "([\\d]{4})-([\\d]{2})-([\\d]{2})";
+		
+		p = Pattern.compile(pattern);
+        m = p.matcher(input);        
+        
+        while (m.find()) 
+        {
+        	obj.add(m.group(1));
+        	obj.add(m.group(2));
+        	obj.add(m.group(3));
+        }
+		
+		return obj;
+	}
+	
+	private void Week_Days(int year, int month, int day)
+	{
+		Calendar myCal = Calendar.getInstance();
+		myCal.set(Calendar.YEAR, year);
+		myCal.set(Calendar.MONTH, month-1);
+		myCal.set(Calendar.DAY_OF_MONTH, day);		
+		myCal.getTime();
+		
+		//DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+		myCal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+		String startDate = "";
+		String endDate = "";
+		startDate = df.format(myCal.getTime());
+		myCal.add(Calendar.DATE, 6);
+		endDate = df.format(myCal.getTime());
+		System.out.println("Start Date = " + startDate);
+		System.out.println("End Date = " + endDate);
+	}
+	
+	private void Today()
+	{
+		Date today = Calendar.getInstance().getTime();
+		String today_str = df.format(today.getTime());
+		System.out.println(today_str);
+	}
+	
+	public static void main(String[] args)
+	{
+		DateTime_function df = new DateTime_function();
+	}
+	
+}
---
+++ src/DateTime/Time_difference.java
@@ -0,0 +1,154 @@
+package DateTime;
+
+/*
+ * version: June 16, 2017 02:26 PM
+ * Last revision: December 15, 2017 11:49 AM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ * Timestamp	https://docs.oracle.com/javase/8/docs/api/java/sql/Timestamp.html
+ * 
+ */
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Time_difference 
+{
+	private String data_now; 	
+	private String data_spec_str = "2017-06-15 18:00:36.629";
+	private String before_day_str = "2017-06-14 18:19:36.629";
+	
+	
+	public Time_difference() throws Exception
+	{		
+		// Now
+		TimeZone tz = TimeZone.getTimeZone("Asia/Taipei");		
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S"); // Quoted "Z" to indicate UTC, no timezone offset
+		df.setTimeZone(tz);
+		String nowAsISO = df.format(new Date());
+		System.out.println(nowAsISO);
+		Date date_format_now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(nowAsISO);
+		System.out.println(date_format_now);
+		
+		// Specified Time
+		Date date_format_spec = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(data_spec_str);
+				
+		// Add time
+		//Add_Month(date_format_spec, 3);
+		//Add_Day(date_format, 7);
+		//Add_Hour(date_format, 10);
+		// Less time
+		//Less_Month(date_format_spec, 3);
+		//Less_Day();
+		
+		// date comparison
+		Date date_format_comparison = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(before_day_str);
+//		Day_compasion(date_format_now, date_format_comparison);		
+		// time different
+//		DateTime_different(date_format_now, date_format_comparison);
+	}
+	
+	private Calendar Add_Month(Date datestr, int month) throws Exception	
+	{		
+		Calendar can_temp = Calendar.getInstance();		
+		can_temp.setTime(datestr);
+		can_temp.add(Calendar.MONTH, +month);
+				
+		return can_temp; 		
+	}
+	
+	private Calendar Add_Day(Date datestr, int day) throws Exception	
+	{
+		//date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(datestr);
+		Calendar can_temp = Calendar.getInstance();		
+		can_temp.setTime(datestr);
+		can_temp.add(Calendar.DAY_OF_MONTH, +day);
+		
+		//System.out.println(can_temp.getTime());
+		return can_temp; 		
+	}
+	
+	private Calendar Add_Hour(Date datestr, int hour) throws Exception
+	{
+		//date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(datestr);
+		Calendar can_temp = Calendar.getInstance();		
+		can_temp.setTime(datestr);
+		can_temp.add(Calendar.HOUR_OF_DAY, +hour);
+		
+		//System.out.println(can_temp.getTime());
+		return can_temp;
+	}
+	
+	private Calendar Less_Month(Date datestr, int month) throws Exception	
+	{
+		Calendar can_temp = Calendar.getInstance();		
+		can_temp.setTime(datestr);
+		can_temp.add(Calendar.MONDAY, -month);
+		
+		System.out.println(can_temp.getTime());
+		return can_temp;
+	}
+	
+	private Calendar Less_Day(Date datestr, int day) throws Exception	
+	{
+		Calendar can_temp = Calendar.getInstance();		
+		can_temp.setTime(datestr);
+		can_temp.add(Calendar.DAY_OF_MONTH, -day);
+		
+		//System.out.println(can_temp.getTime());
+		return can_temp;
+	}
+	
+	private void Day_compasion(Date data_now, Date data_comparison) throws Exception
+	{
+		//Date date_format_now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(data_now);
+		//Date date_format_comparison = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(data_comparison);
+		
+		System.out.println(data_now);
+		System.out.println(data_comparison);
+		
+		if(data_now.after(data_comparison)){
+			System.out.println("data_now > data_comparison");
+		}else{
+			System.out.println("data_now < data_comparison");
+		}
+	}
+	
+	private void DateTime_different(Date data_now, Date data_comparison)
+	{        
+        try {
+            long diff = data_comparison.getTime() - data_now.getTime();
+            
+            long diffSeconds = diff / 1000 % 60;
+            long diffMinutes = diff / (60 * 1000) % 60;
+            long diffHours = diff / (60 * 60 * 1000) % 24;
+            long diffDays = diff / (24 * 60 * 60 * 1000);
+
+            System.out.print(diffDays + " days, ");
+            System.out.print(diffHours + " hours, ");
+            System.out.print(diffMinutes + " minutes, ");
+            System.out.print(diffSeconds + " seconds.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+	}
+	
+	public static void main(String args[]) 
+	{
+		try {
+			Time_difference tf = new Time_difference();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
