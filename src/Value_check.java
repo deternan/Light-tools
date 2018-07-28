82a1e528cd85b7f4bef15982d7f5d25504ca1b2f
deternan <chke@iir.csie.ncku.edu.tw>
2018-07-28 12:15:14 +0800

    value check

---
+++ src/Value_check.java
@@ -0,0 +1,41 @@
+
+/* 
+ * version: July 28, 2018 12:11 PM
+ * Last revision: July 28, 2018 12:11 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+public class Value_check 
+{
+
+	public Value_check()
+	{
+		NaN_check();
+				
+		
+	}
+	
+	private void NaN_check()
+	{
+		double aa = 0.0;
+		double divided = 0;
+		
+		double result = aa / divided;
+		
+		if(Double.isNaN(result)){
+			System.out.println("NaN");
+		}else{
+			System.out.println(aa);
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		Value_check vc = new Value_check();
+	}
+	
+}
