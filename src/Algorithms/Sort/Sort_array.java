e0e327ec2f288f80ed1a1fb10afd7b2b94912caf
DELTA\BARRY.KE <barry.ke@deltaww.com>
2018-09-20 16:55:07 +0800

    change folder

---
+++ src/Algorithms/Sort/Sort_array.java
@@ -0,0 +1,24 @@
+package Algorithms.Sort;
+import java.util.Arrays;
+
+public class Sort_array 
+{
+
+	public Sort_array()
+	{
+		int[] arr = {13, 7, 6, 45, 21, 9, 101, 102};
+		 
+        Arrays.sort(arr);
+ 
+        for(int i=0; i<arr.length; i++)
+        {
+        	System.out.println(arr[i]);
+        }
+	}
+	
+	public static void main(String[] args)
+	{
+		Sort_array sort = new Sort_array();
+	}
+	
+}
