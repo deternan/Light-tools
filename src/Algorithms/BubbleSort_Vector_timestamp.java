d6a753a52f05cba3720b8f171f8251dd7af79cf7
DELTA\BARRY.KE <barry.ke@deltaww.com>
2018-09-20 17:24:42 +0800

    change LocalDate to LocalDateTime

--- src/Algorithms/BubbleSort_Vector_timestamp.java
+++ src/Algorithms/BubbleSort_Vector_timestamp.java
@@ -1,14 +1,10 @@
 package Algorithms;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 /*
  * Bobble Sort (Vector) - timestamp
  * 
  * version: September 20, 2018 04:53 PM
- * Last revision: September 20, 2018 04:53 PM
+ * Last revision: September 20, 2018 05:22 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -16,9 +12,12 @@ import java.util.Locale;
  * 
  */
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class BubbleSort_Vector_timestamp 
 {
-//	private Vector timestamp = new Vector();
 	private String[] timestamp;
 	DateTimeFormatter formatter;
 	private String isotime_pattern = "EEE MMM dd HH:mm:ss zzz yyyy";
@@ -27,13 +26,14 @@ public class BubbleSort_Vector_timestamp
 	
 	public BubbleSort_Vector_timestamp()
 	{
-		timestamp = new String[5];
+		timestamp = new String[6];
 		
 		timestamp[0] = "Mon Jul 30 14:25:53 CST 2018";
 		timestamp[1] = "Mon Aug 27 21:11:01 CST 2018";
 		timestamp[2] = "Tue Dec 12 05:03:19 CST 2017";
 		timestamp[3] = "Fri Jan 27 12:57:32 CST 2017";
 		timestamp[4] = "Tue Jul 24 14:42:30 CST 2018";
+		timestamp[5] = "Tue Jul 24 15:22:12 CST 2018";
 		
 		formatter = DateTimeFormatter.ofPattern(isotime_pattern, Locale.ENGLISH);
 		 
@@ -42,13 +42,13 @@ public class BubbleSort_Vector_timestamp
 		
 		for(int i=0; i<timestamp.length; i++)
 		{
-			LocalDate timestamp_i = LocalDate.parse(timestamp[i], formatter);
+			LocalDateTime timestamp_i = LocalDateTime.parse(timestamp[i], formatter);
 			
 			j = i;
 		    for(int k=i; k<timestamp.length; k++)
 		    {
-		    	LocalDate timestamp_j = LocalDate.parse(timestamp[j], formatter);
-		    	LocalDate timestamp_k = LocalDate.parse(timestamp[k], formatter);
+		    	LocalDateTime timestamp_j = LocalDateTime.parse(timestamp[j], formatter);
+		    	LocalDateTime timestamp_k = LocalDateTime.parse(timestamp[k], formatter);
 		    	if(timestamp_j.isBefore(timestamp_k)) {
 		    		j = k;
 		    	}
