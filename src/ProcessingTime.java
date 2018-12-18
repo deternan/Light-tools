992ec26c64445947d32aa4473480294a8b818c44
DELTA\BARRY.KE <barry.ke@deltaww.com>
2018-12-18 09:40:16 +0800

    processing time

---
+++ src/ProcessingTime.java
@@ -0,0 +1,42 @@
+
+/*
+ * time gap
+ * 
+ * version: December 18, 2018 09:38 AM
+ * Last revision: December 18, 2018 09:38 AM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+public class ProcessingTime 
+{
+
+	public ProcessingTime()
+	{
+		long startTime = System.currentTimeMillis();
+		
+		// Processing
+		for(int i=1; i<=9; i++)
+		{
+			for(int j=1; j<=9; j++)
+			{
+				System.out.println(i+"x"+j+"="+(i*j));
+			}			
+		}
+		
+		long endTime = System.currentTimeMillis();
+		
+		long timegap = endTime - startTime;
+		
+		System.out.println(timegap);
+	}
+	
+	public static void main(String[] args)
+	{
+		ProcessingTime PT = new ProcessingTime();
+	}
+	
+}
