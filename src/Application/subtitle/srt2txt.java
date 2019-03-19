0ab5005c2a68ced9eab163682c76efa9bb0c421e
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-03-19 17:31:26 +0800

    srt to pure text

--- src/Application/subtitle/srt2txt.java
+++ src/Application/subtitle/srt2txt.java
@@ -1,5 +1,17 @@
 package Application.subtitle;
 
+/*
+ * Transfer *.str to *.txt format
+ * 
+ * version: October 25, 2017 10:43 AM
+ * Last revision: March 19, 2019 05:22 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -10,30 +22,19 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 
-/*
- * Transfer *.str to *.txt format
- * 
- * version: October 25, 2017 10:43 AM
- * Last revision: October 25, 2017 11:31 AM
- * 
- * Author : Chao-Hsuan Ke
- * Institute: Delta Research Center
- * Company : Delta Electronics Inc. (Taiwan)
- * 
- */
-
 public class srt2txt 
 {
 	// Read files
-	private String folder_source = "C:\\Users\\Barry.Ke\\Desktop\\全部字幕\\深入淺出 VR 虛擬實境開發 (3)\\";
-	private String file = "";
+	private String folder_source = "";	
 	private BufferedReader bfr;
 	
 	// Write output
-	private String output_folder = "C:\\Users\\Barry.Ke\\Desktop\\全部字幕 (txt)\\深入淺出 VR 虛擬實境開發 (3)\\";
+	private String output_folder = "";
 	private String output_file;
 	
 	private String sentence;
+	
+	Vector oriVec = new Vector();
 	
 	public srt2txt() throws Exception
 	{
@@ -45,7 +46,7 @@ public class srt2txt
 
 		for (File file : listOfFiles) {
 			if (file.isFile()) {				
-				System.out.println(file.getName());
+				//System.out.println(file.getName());
 				output_file = file.getName() + ".txt";
 				Parser_subtitle(folder_source + file.getName());
 				// System.out.println(total_str);
@@ -64,27 +65,53 @@ public class srt2txt
 		bfr = new BufferedReader(fr);
 		String temp;
 		while((Line = bfr.readLine())!=null)
-		{						
-			//System.out.println(Line);
-			
-			if(CJKV_check(Line) == true){
-				//System.out.println(Line);
-				sentence += Line +"\n";
+		{									
+//			if(CJKV_check(Line) == true)
+			{
+				temp = new String(Line.getBytes("utf-8"), "utf-8");
+				//System.out.println(new String(Line.getBytes("utf-8"), "utf-8"));
+				//System.out.println(temp);
+				filter(temp);				
 			}
-		}
-		
+		}		
 		fr.close();
-		bfr.close();
+		bfr.close();		
 	}
 	
-	private boolean CJKV_check(String input_str)
-	{		
-		boolean check;
-		check = input_str.codePoints().anyMatch(codepoint ->
-	            Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN);
+	private void filter(String inputStr) {
+		boolean lengthCheck = false;
+		boolean tagCheck = false;
+		boolean numCheck = false;
 		
-		return check;
+		// length check
+		if(inputStr.trim().length() > 0) {
+			//System.out.println(inputStr);
+			lengthCheck = true;
+		}
+		// Tag check
+		if(inputStr.contains("-->") == false) {
+			tagCheck = true;
+		}
+		// Numerical check
+		if((inputStr.trim().matches("[0-9]+") == false) && (inputStr.trim().length() >3)) {
+			numCheck = true;
+		}
+		
+		if((lengthCheck == true) && (tagCheck == true) && (numCheck == true)) {
+			//System.out.println(inputStr);
+			sentence += inputStr +"\n";
+		}
+		
 	}
+	
+//	private boolean CJKV_check(String input_str)
+//	{		
+//		boolean check;
+//		check = input_str.codePoints().anyMatch(codepoint ->
+//	            Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN);
+//		
+//		return check;
+//	}
 	
 	private void Output_txt(String outputfilename, String str)
 	{
