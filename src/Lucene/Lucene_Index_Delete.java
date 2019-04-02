23c2af72906b00c4eb18a6ef10a651279b7e508d
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-04-02 09:41:29 +0800

    Lucene delete index

--- src/Lucene/Lucene_Index_Delete.java
+++ src/Lucene/Lucene_Index_Delete.java
@@ -1,53 +1,53 @@
-package Lucene;
+package Update.Search.Data;
+
+/*
+ *  Lucene Index (Delete)
+ * 
+ *	Version: April 02, 2019 09:09 AM
+ * 	Last revision: April 02, 2019 09:09 AM
+ * 	
+ */
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+public class Lucene_Index_Delete  extends Parameters {
 
-public class Lucene_Index_Delete 
-{
-	private String indexFilePath = "";
+	String deleteId = "5566";
+	
+	// Lucene Index	
+	IndexWriter indexWriter;
+	Analyzer analyzer = new StandardAnalyzer();
+	private static Directory dir = null;
 	
 	public Lucene_Index_Delete()
 	{
-		try{		   
-			
-			Path path = Paths.get(indexFilePath);
-			Directory index = FSDirectory.open(path);
-			StandardAnalyzer analyzer = new StandardAnalyzer();
-						
-			Document doc = new Document();
-			doc.add(new TextField("alias", "SCORM.pdf", Field.Store.YES));
-			doc.add(new TextField("filename", "03684a50ddb5a8e735d4fb17a1599ed3.pdf", Field.Store.YES));
-			doc.add(new TextField("resource_path", "/media/03684a50ddb5a8e735d4fb17a1599ed3.pdf", Field.Store.YES));
-			
-			//IndexWriter writer = new IndexWriter(index, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
-			
-			
-		    System.out.println("Delete Finished");
-		    
-		  }
-		  catch(Exception x){x.printStackTrace();
-		  }
+		// Lucene
+		File f = new File(lucene_index);
+		try {
+			dir = FSDirectory.open(f.toPath());
+			indexWriter = new IndexWriter(dir, new IndexWriterConfig(analyzer));
+			Term term = new Term(itemId_Tag, deleteId);
+            indexWriter.deleteDocuments(term);
+            indexWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	public static void main(String[] args)
 	{
-		Lucene_Index_Delete LID = new Lucene_Index_Delete();
+		Lucene_Index_Delete DLI = new Lucene_Index_Delete();
 	}
+	
 }
