eb3e84d6f39c61f9c514405d228de9bf10a1593b
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-04-24 10:44:39 +0800

    POS (English)

---
+++ src/Text-related/Stanford/English_POS.java
@@ -0,0 +1,38 @@
+package Text_related.Stanford;
+
+/*
+ * Auto-Updated auto-completed Items 
+ * 
+ * version: August 24, 2019 09:56 AM
+ * Last revision: April 24, 2019 10:38 AM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+public class English_POS {
+
+	public English_POS()	
+	{
+		// Initialize the tagger
+        MaxentTagger tagger = new MaxentTagger("taggers/english-left3words-distsim.tagger");
+ 
+        // The sample string
+        String sample = "This is a sample text";
+ 
+        // The tagged string
+        String tagged = tagger.tagString(sample);
+ 
+        // Output the result
+        System.out.println(tagged);
+	}
+	
+	public static void main(String[] args)
+	{
+		English_POS pos = new English_POS();
+	}
+}
