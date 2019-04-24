76d87fa1b42dbb4cb6030013186cb5f307e28c18
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-04-24 16:11:40 +0800

    Chinese character Segmentation

--- src/Text-related/Stanford/Chinese_Seg.java
+++ src/Text-related/Stanford/Chinese_Seg.java
@@ -1,10 +1,10 @@
 package Text_related.Stanford;
 
 /*
- * Auto-Updated auto-completed Items 
+ * Chinese character Segmentation 
  * 
  * version: August 22, 2019 09:56 AM
- * Last revision: April 23, 2019 10:21 AM
+ * Last revision: April 24, 2019 04:09 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -16,6 +16,7 @@ package Text_related.Stanford;
 import java.io.*;
 import java.util.List;
 import java.util.Properties;
+import java.util.Vector;
 
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.ling.CoreLabel;
@@ -37,46 +38,82 @@ import edu.stanford.nlp.ling.CoreLabel;
 public class Chinese_Seg {
 
 //  private static final String basedir = System.getProperty("SegDemo", "data");
-	private static final String basedir = "C:\\Users\\barry.ke\\Desktop\\stanford-segmenter-2018-10-16\\data\\";
+	private static final String basedir = "";	// data path
 	
 	static List<String> segmented;
+	CRFClassifier<CoreLabel> segmenter;
+	
+	// Segmented Terms
+	Vector segTrems = new Vector();
+	
+	public Chinese_Seg() throws Exception
+	{
+		System.setOut(new PrintStream(System.out, true, "utf-8"));
+
+	    Properties props = new Properties();
+	    props.setProperty("sighanCorporaDict", basedir);
+	    // props.setProperty("NormalizationTable", "data/norm.simp.utf8");
+	    // props.setProperty("normTableEncoding", "UTF-8");
+	    
+	    props.setProperty("serDictionary", basedir + "/dict-chris6.ser.gz");
+//	    if (args.length > 0) {
+//	      props.setProperty("testFile", args[0]);
+//	    }
+	    props.setProperty("inputEncoding", "UTF-8");
+	    props.setProperty("sighanPostProcessing", "true");
+
+	    segmenter = new CRFClassifier<>(props);
+	    segmenter.loadClassifierNoExceptions(basedir + "/ctb.gz", props);
+//	    for (String filename : args) {
+//	      segmenter.classifyAndWriteAnswers(filename);
+//	    }
+	    
+	    String sample = "我住在美国。";
+	    segmented = segmenter.segmentString(sample);
+	    System.out.println(segmented);
+	    
+	    Terms_Split(segmented);
+	    
+	    sample = "超人氣珍珠泡芙怎麼咬都爆漿！IG南北兩大夯店推薦";
+	    segmented = segmenter.segmentString(sample);
+	    System.out.println(segmented);
+	    
+	    Terms_Split(segmented);
+	    
+	    sample = "房子，在中国具有不一般的意义";
+	    segmented = segmenter.segmentString(sample);
+	    System.out.println(segmented);
+	    
+	    Terms_Split(segmented);
+	    
+	    System.out.println(segTrems.size());
+	    for(int i=0; i<segTrems.size(); i++) {
+	    	System.out.println(segTrems.get(i));
+	    }
+	    
+	}
+	
+	private void Terms_Split(List<String> listStr)
+	{
+		for(int i=0; i<listStr.size(); i++) {
+			
+			// Filter (length>1)
+			if(listStr.get(i).toString().trim().length() > 1) {
+				segTrems.add(listStr.get(i));
+			}			
+		}		
+	}
+	
 	
   public static void main(String[] args) throws Exception {
 	  
-    System.setOut(new PrintStream(System.out, true, "utf-8"));
+	  Chinese_Seg seg = new Chinese_Seg();
 
-    Properties props = new Properties();
-    props.setProperty("sighanCorporaDict", basedir);
-    // props.setProperty("NormalizationTable", "data/norm.simp.utf8");
-    // props.setProperty("normTableEncoding", "UTF-8");
     
-    props.setProperty("serDictionary", basedir + "/dict-chris6.ser.gz");
-    if (args.length > 0) {
-      props.setProperty("testFile", args[0]);
-    }
-    props.setProperty("inputEncoding", "UTF-8");
-    props.setProperty("sighanPostProcessing", "true");
-
-    CRFClassifier<CoreLabel> segmenter = new CRFClassifier<>(props);
-    segmenter.loadClassifierNoExceptions(basedir + "/ctb.gz", props);
-    for (String filename : args) {
-      segmenter.classifyAndWriteAnswers(filename);
-    }
-
-    String sample = "我住在美国。";
-    segmented = segmenter.segmentString(sample);
-    System.out.println(segmented);
-    //segmented.clear();
-    
-    sample = "超人氣珍珠泡芙怎麼咬都爆漿！IG南北兩大夯店推薦";
-    segmented = segmenter.segmentString(sample);
-    System.out.println(segmented);
-    
-    sample = "房子，在中国具有不一般的意义";
-    segmented = segmenter.segmentString(sample);
-    System.out.println(segmented);
     
   }
+  
+  
 
 }
 
