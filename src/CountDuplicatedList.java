8cfef4f1296857ddb915f6a3e0138c52426034db
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-05-03 16:58:19 +0800

    Count DuplicatedList

--- src/CountDuplicatedList.java
+++ src/CountDuplicatedList.java
@@ -1,47 +1,54 @@
 
+/*
+ * CountDuplicatedList
+ * 
+ * version: May 02, 2019 06:40 PM
+ * Last revision: May 02, 2019 07:00 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * Institute: Delta Research Center
+ * Company : Delta Electronics Inc. (Taiwan)
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
- 
 public class CountDuplicatedList {
- 
-	public CountDuplicatedList()
-	{
-		
+
+	public CountDuplicatedList() {
+
 		List<String> list = new ArrayList<String>();
-	      list.add("Ashok");
-	      list.add("Vinod");
-	      list.add("Kumar");
-	      list.add("Ashok");
-	      list.add("Mariyala");
-	      list.add("Venkata");
-	      list.add("Mariyala");
-	      list.add("Vinod");
-	      list.add("Ashok");
-	      list.add("Kumar");
-	 
-	      Map<String, Integer> duplicates = new HashMap<String, Integer>();
-	 
-	      for (String str : list) {
-	         if (duplicates.containsKey(str)) {
-	            duplicates.put(str, duplicates.get(str)+1);
-	         } else {
-	            duplicates.put(str, 1);
-	         }
-	      }
-	 
-	      
-	      
-	      for (Map.Entry<String, Integer> entry : duplicates.entrySet()) {
-	         System.out.println(entry.getKey() + " = " + entry.getValue());
-	      }
+		list.add("Ashok");
+		list.add("Vinod");
+		list.add("Kumar");
+		list.add("Ashok");
+		list.add("Mariyala");
+		list.add("Venkata");
+		list.add("Mariyala");
+		list.add("Vinod");
+		list.add("Ashok");
+		list.add("Kumar");
+
+		Map<String, Integer> duplicates = new HashMap<String, Integer>();
+
+		for (String str : list) {
+			if (duplicates.containsKey(str)) {
+				duplicates.put(str, duplicates.get(str) + 1);
+			} else {
+				duplicates.put(str, 1);
+			}
+		}
+
+		for (Map.Entry<String, Integer> entry : duplicates.entrySet()) {
+			System.out.println(entry.getKey() + " = " + entry.getValue());
+		}
 	}
-	
-  public static void main(String[] args) {
-	  CountDuplicatedList countList = new CountDuplicatedList();
-  }
- 
- 
+
+	public static void main(String[] args) {
+		CountDuplicatedList countList = new CountDuplicatedList();
+	}
+
 }
