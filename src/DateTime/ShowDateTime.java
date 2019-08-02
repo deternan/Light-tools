4d95b9bfd887b318fd38f2d2409abda32841e6ed
Phelps <chke@iir.csie.ncku.edu.tw>
2019-08-02 14:06:15 +0800

    revised datetime function

--- src/DateTime/ShowDateTime.java
+++ src/DateTime/ShowDateTime.java
@@ -3,7 +3,7 @@ package DateTime;
 /*
  * Show DataTime
  * version: April 16, 2019 11:54 AM
- * Last revision: April 16, 2019 12:04 PM	
+ * Last revision: August 02, 2019 01:36 PM	
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -11,22 +11,50 @@ package DateTime;
  * 
  */
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class ShowDateTime {
 
-	public ShowDateTime()
+	//private String DateTimeFormat = "yyyy/MM/dd hh:mm:ss";
+	private String DateTimeFormat = "EEE MMM dd HH:mm:ss zzz yyyy";
+	
+	public ShowDateTime() throws Exception
 	{
-		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-		Date date = new Date();
-		String strDate = sdFormat.format(date);
-		System.out.println(strDate);
+		// Function 1
+//		SimpleDateFormat sdFormat = new SimpleDateFormat(DateTimeFormat);
+//		Date date = new Date();
+//		String strDate = sdFormat.format(date);
+//		System.out.println(strDate);
+		
+		// Function 2
+//		TimeZone tz = TimeZone.getTimeZone("Asia/Taipei");
+//		DateFormat df = new SimpleDateFormat(DateTimeFormat);
+//		df.setTimeZone(tz);
+//		String nowAsISO = df.format(new Date());
+//		System.out.println(nowAsISO);
+		
+		// Function 3
+		
+		
+		SimpleDateFormat myDate = new SimpleDateFormat(DateTimeFormat);
+		myDate.setTimeZone(TimeZone.getTimeZone("Asia/Taipei"));
+		String nowAsISO = myDate.format(new Date());
+		Date newDate = myDate.parse(nowAsISO);
+		System.out.println(newDate);
 	}
 	
 	public static void main(String[] args)
 	{
-		ShowDateTime show = new ShowDateTime();
+		try {
+			ShowDateTime show = new ShowDateTime();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
